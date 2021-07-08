b9eb853b3340558a6431cc3de5d8df7ebb77ddc1
MelhOu <michelle.laraherrera@gmail.com>
2021-07-07 23:56:59 -0500

    tarea

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 //archivo de pruebas
+
 function name(){
     console.log("hola mundo cochino");
 };
+
+function suma(a,b){
+    var resultado = a+b;
+    console.log("resultado: " + resultado);
+}
+//ingresar un numero y decirme si soy mayo o menor
+function comparar(edad){
+    if(edad>=18){
+        console.log("mayor de edad");
+    }else{
+        console.log("menor de edad");
+    }
+
+}
+
+comparar(0);
+
+
